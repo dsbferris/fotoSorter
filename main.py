2381c495f394cd43b07d67ae689185e1e146843b
Ferris Schwanebeck <accounts@ferris-s.de>
2022-07-31 14:39:30 +0200

    Working on it...

--- main.py
+++ main.py
@@ -100,6 +100,7 @@ def move_files_with_their_json(path: str):
             # skip this
             pass
         else:
+
             # TODO Handle files with "...-bearbeitet..."
             raise NotImplementedError
 
